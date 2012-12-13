1af226a0a49179b3e1907765099533488a320894
Brandon Donnelson <branflake2267@gmail.com>
2012-12-13 09:44:24 -0800

    Tooltip test formatting

--- tooltip/plugin/src/test/java/com/arcbees/gquery/tooltip/client/TooltipTest.java
+++ tooltip/plugin/src/test/java/com/arcbees/gquery/tooltip/client/TooltipTest.java
@@ -1,18 +1,17 @@
 package com.arcbees.gquery.tooltip.client;
 
 import com.google.gwt.junit.client.GWTTestCase;
+
 /**
  * Test class for Tooltip plugin
  */
 public class TooltipTest extends GWTTestCase {
-
   public String getModuleName() {
     return "com.arcbees.gquery.tooltip.Tooltip";
   }
 
   public void testTooltipApply() {
-    
-    //TODO
+    // TODO
+    assertTrue(true);
   }
-
 }
