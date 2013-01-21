809ef680552c4315797b37d40a127f52b00c1500
jdramaix <julien.dramaix@gmail.com>
2013-01-21 12:30:20 +0100

    fix bug + read delay options from data

--- tooltip/src/main/java/com/arcbees/gquery/tooltip/client/TooltipImpl.java
+++ tooltip/src/main/java/com/arcbees/gquery/tooltip/client/TooltipImpl.java
@@ -323,7 +323,8 @@ public class TooltipImpl {
         }
 
         //read data-* attributes on element
-        options.withHtml(readDataAttributes("animation", options.isAnimation(), new BooleanConverter()));
+        options.withAnimation(readDataAttributes("animation", options.isAnimation(), new BooleanConverter()));
+        options.withDelay(readDataAttributes("delay", options.getDelayShow(), new IntegerConverter()));
         options.withDelayHide(readDataAttributes("delayHide", options.getDelayHide(), new IntegerConverter()));
         options.withDelayShow(readDataAttributes("delayShow", options.getDelayShow(), new IntegerConverter()));
         options.withHtml(readDataAttributes("html", options.isHtml(), new BooleanConverter()));
